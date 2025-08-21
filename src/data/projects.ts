11bd603be4ae5c8a9c4a3ee2de8a0d827b5ce0de
Reece Leneveu <reeceleneveu@gmail.com>
2025-08-21 02:07:27 +0100

    Added payment method”

---
+++ src/data/projects.ts
@@ -0,0 +1,17 @@
+export type Project = {
+	name: string
+	description: string
+	imageUrl: string
+	link?: string
+}
+
+export const projects: Project[] = [
+	{
+		name: 'Star Wars UI',
+		description: 'Star Wars Inspired UI with a HUD and an information frame.',
+		imageUrl: '/portfolio/korriban.png',
+		link: '#',
+	},
+]
+
+
